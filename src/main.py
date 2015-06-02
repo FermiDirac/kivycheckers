bec69ac4e27c22edc6e84f9f23acb50747040171
FermiDirac <scottfennelly@gmail.com>
2015-06-01 23:56:16 -0400

    Grid coloring proof of concept

--- src/main.py
+++ src/main.py
@@ -1,14 +1,26 @@
-__author__ = 'Scott'
 from kivy.app import App
-from kivy.uix.widget import Widget
+from kivy.uix.gridlayout import GridLayout 
+from kivy.uix.widget import Widget 
+from kivy.uix.button import Button
+from kivy.graphics import *
 
+class CheckersSquare(Widget):
+    pass
 
 class CheckersGame(GridLayout):
-    pass
+    def __init__(self):
+        super(CheckersGame, self).__init__()
+        self.cols = 8
+
+    def make_board(self):
+        for i in range(64):
+            self.add_widget(CheckersSquare())
 
 class CheckersApp(App):
     def build(self):
-        return CheckersGame().make_board()
+        game = CheckersGame()
+        game.make_board()
+        return game
 
 if __name__ == '__main__':
     CheckersApp().run()
