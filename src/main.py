e7aa4b615619286450b182276d6fc19aac0f59cb
FermiDirac <scottfennelly@gmail.com>
2015-06-09 22:42:45 -0500

    Pieces now display properly

--- src/main.py
+++ src/main.py
@@ -1,22 +1,30 @@
 from kivy.app import App
 from kivy.uix.gridlayout import GridLayout 
+from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.widget import Widget 
+from kivy.uix.button import Button 
 from kivy.graphics import *
 from itertools import product
 
-class WhiteSquare(Widget):
+class WhitePiece(Widget):
     pass
 
-class BlackSquare(Widget):
+class RedPiece(Widget):
+    pass
+
+class Square(BoxLayout):
     pass
 
 class CheckersGame(GridLayout):
     def make_board(self):
+        for i in range(64):
+            self.add_widget(Square())
         for row, col in product(range(8), repeat = 2):
-            if (row + col) % 2 == 0:
-                self.add_widget(WhiteSquare())
-            else:
-                self.add_widget(BlackSquare())
+            if (row + col) % 2 == 0 and row not in (3, 4):
+                if (row * 8 + col) < 23:
+                    self.children[row*8 + col].add_widget(WhitePiece())
+                else:
+                    self.children[row*8 + col].add_widget(RedPiece())
 
 
 class CheckersApp(App):
